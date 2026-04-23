a6d286a3f8694543732ba78d2a958c21845d65a8
kiaras-create <ksunilsingh@smith.edu>
2026-04-23 16:10:33 -0400

    added url for llama

--- chat_bot.py
+++ chat_bot.py
@@ -1,21 +1,22 @@
 import requests
 
 def askLlama(userMessage):
-    url = "http://localhost:11434/api/chat"
+    urls = ["http://localhost:11434/api/chat",
+        "https://roboadvisor-gui-mdayryx2vw6ya79omujfoq.streamlit.app/"]
 
     payload = {
         "model": "mistral",
         "messages": userMessage,
         "stream": False
     }
-
-    try:
-        response = requests.post(url, json = payload)
-        # response.raise_for_status
-        # return response.json()['message']['content']
-        data = response.json()
-        return data["message"]["content"]
-    
-    except requests.exceptions.RequestException as e:
-        return f"Error talking to Llama3 {e}"
+    for url in urls:
+        try:
+            response = requests.post(url, json = payload)
+            # response.raise_for_status
+            # return response.json()['message']['content']
+            data = response.json()
+            return data["message"]["content"]
+        
+        except requests.exceptions.RequestException as e:
+            return f"Error talking to Llama3 {e}"
 
