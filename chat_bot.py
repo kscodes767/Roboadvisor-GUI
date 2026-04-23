9ce906e9e1e0746aa30d00a13673a591c4c638ab
kiaras-create <ksunilsingh@smith.edu>
2026-04-23 16:15:19 -0400

    removed llama for streamlit

--- chat_bot.py
+++ chat_bot.py
@@ -1,22 +1,25 @@
 import requests
 
 def askLlama(userMessage):
-    urls = ["http://localhost:11434/api/chat",
-        "https://roboadvisor-gui-mdayryx2vw6ya79omujfoq.streamlit.app/"]
+    urls = "http://localhost:11434/api/chat"
+        
 
     payload = {
         "model": "mistral",
         "messages": userMessage,
         "stream": False
     }
-    for url in urls:
-        try:
-            response = requests.post(url, json = payload)
+
+    try:
+        response = requests.post(url, json = payload)
             # response.raise_for_status
             # return response.json()['message']['content']
-            data = response.json()
-            return data["message"]["content"]
+        data = response.json()
+        return data["message"]["content"]
+    except requests.exceptions.RequestException:
+
+        return "Chatbot is only available in the local version while Ollama is running."
         
-        except requests.exceptions.RequestException as e:
-            return f"Error talking to Llama3 {e}"
+    except requests.exceptions.RequestException as e:
+        return f"Error talking to Llama3 {e}"
 
